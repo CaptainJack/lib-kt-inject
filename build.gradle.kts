0a0f23f3deb660b2fcc2eef4866ab81ac806db11
Alexey Chuprin <a.chuprin@capjack.ru>
2021-05-11 16:38:55 +0300

    Kotlin 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,33 @@
 plugins {
-	kotlin("multiplatform") version "1.4.10"
-	id("nebula.release") version "15.3.0"
-	id("ru.capjack.bintray") version "1.0.0"
+	kotlin("multiplatform") version "1.5.0"
+	id("ru.capjack.publisher") version "0.1.0"
 }
 
-allprojects {
-	group = "ru.capjack.tool"
-	repositories {
-		jcenter()
-		mavenLocal()
-		maven("https://dl.bintray.com/capjack/public")
-	}
+group = "ru.capjack.tool"
+
+repositories {
+	mavenCentral()
+	mavenCapjack()
 }
 
 kotlin {
 	jvm {
-		compilations.all { kotlinOptions.jvmTarget = "1.8" }
+		compilations.all { kotlinOptions.jvmTarget = "11" }
 	}
 	
 	sourceSets {
 		get("commonMain").dependencies {
-			implementation("ru.capjack.tool:tool-reflect:1.2.0")
-			implementation("ru.capjack.tool:tool-logging:1.2.0")
+			implementation("ru.capjack.tool:tool-reflect:1.3.0")
+			implementation("ru.capjack.tool:tool-logging:1.5.0")
 		}
 		get("commonTest").dependencies {
-			implementation(kotlin("test-common"))
-			implementation(kotlin("test-annotations-common"))
+			implementation(kotlin("test"))
 		}
 		
 		get("jvmMain").dependencies {
 			implementation(kotlin("reflect"))
 		}
 		get("jvmTest").dependencies {
-			implementation(kotlin("test-junit"))
 			implementation("ch.qos.logback:logback-classic:1.2.+")
 		}
 	}
